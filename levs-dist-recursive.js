57d85ceb61fe43b79085a3954a0d07179b289a3a
Sandra <missin_link_x@yahoo.com>
2017-02-15 11:10:52 +0200

    added jQuery, works with levs-html-page

--- levs-dist-recursive.js
+++ levs-dist-recursive.js
@@ -1,28 +1,35 @@
-var fs = require("fs")
-var str1 = "art";
-var len1 = str1.length;
-var str2 = "math";
-var len2 = str2.length;
+$(document).ready(function () {
+  var str1,len1,str2,len2;
 
-function levsDist (str1, len1, str2, len2) {
-  var cost;
+  function levsDist (str1, len1, str2, len2) {
+    var cost;
 
-  if (len1 === 0) {
-    return len2;
+    if (len1 === 0) {
+      return len2;
+    }
+    if (len2 === 0) {
+      return len1;
+    }
+
+    if (str1[len1-1] === str2[len2-1]) {
+      cost = 0;
+    } else {
+      cost = 1;
+    }
+
+    return Math.min(levsDist(str1, len1-1, str2, len2) + 1,
+               levsDist(str1, len1, str2, len2-1) +1,
+               levsDist(str1, len1-1, str2, len2-1) + cost);
   }
-  if (len2 === 0) {
-    return len1;
-  }
 
-  if (str1[len1-1] === str2[len2-1]) {
-    cost = 0;
-  } else {
-    cost = 1;
-  }
-
-  return Math.min(levsDist(str1, len1-1, str2, len2) + 1,
-             levsDist(str1, len1, str2, len2-1) +1,
-             levsDist(str1, len1-1, str2, len2-1) + cost);
-}
-
-console.log(levsDist(str1,len1,str2,len2))
+    $('#calculator').on('keyup',function () {
+      str1 = $('#str1').val();
+      len1 = str1.length;
+      str2 = $('#str2').val();
+      len2 = str2.length;
+      $('#derp').text(str1+len1);
+      $('label').text("Lev's distance between " +$('#str1').val()+" and "+$('#str2').val()+" is:")
+      var dist = levsDist(str1,len1,str2,len2);
+      $('#distance').text(+dist);
+    });
+});
